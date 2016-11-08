openbmc/openbmc

Add recipe to install device tree generator script
Change-Id: I70677db17e3fef94243b1d77ffd54c897ab2f6f8
Signed-off-by: Matt Spinler <spinler@us.ibm.com>

meta-phosphor/common/recipes-phosphor/mrw/mrw-devtree-native.bb
@@ -1,0 +1,19 @@
+SUMMARY = "Phosphor Device Tree Generator"
+DESCRIPTION = "Script to generate the BMC device tree from the MRW XML"
+PR = "r1"
+
+S = "${WORKDIR}/git"
+
+inherit obmc-phosphor-license
+inherit native
+inherit mrw-rev
+
+DEPENDS += "yaml-tiny mrw-api-native"
+
+SRC_URI += "${MRW_DEVTREE_SRC_URI}"
+SRCREV = "${MRW_DEVTREE_SRCREV}"
+
+do_install() {
+    install -d ${D}${STAGING_BINDIR_NATIVE}
+    install -m 0755 gen_devtree.pl ${D}${STAGING_BINDIR_NATIVE}/gen_devtree.pl
+}

openbmc/meta-phosphor

common/recipes-phosphor/mrw/mrw-devtree-native.bb
@@ -1,0 +1,19 @@
+SUMMARY = "Phosphor Device Tree Generator"
+DESCRIPTION = "Script to generate the BMC device tree from the MRW XML"
+PR = "r1"
+
+S = "${WORKDIR}/git"
+
+inherit obmc-phosphor-license
+inherit native
+inherit mrw-rev
+
+DEPENDS += "yaml-tiny mrw-api-native"
+
+SRC_URI += "${MRW_TOOLS_SRC_URI}"
+SRCREV = "${MRW_TOOLS_SRCREV}"
+
+do_install() {
+    install -d ${D}${STAGING_BINDIR_NATIVE}
+    install -m 0755 gen_devtree.pl ${D}${STAGING_BINDIR_NATIVE}/gen_devtree.pl
+}