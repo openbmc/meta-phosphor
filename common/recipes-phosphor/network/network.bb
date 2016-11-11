openbmc/openbmc

Install the network-manager conf file in sysconf
Fixes openbmc/openbmc#622

Change-Id: I559f5771102c79b3f13c9c0323c12c03fa51684f
Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

meta-phosphor/common/recipes-phosphor/network/network.bb
@@ -21,4 +21,5 @@ S = "${WORKDIR}/git"
 do_install_append() {
         install -d ${D}/${sbindir}
         install ${S}/netman.py ${D}/${sbindir}
+        install ${S}/conf/network-manager.ini ${D}/${sysconfdir}
 }

openbmc/meta-phosphor

common/recipes-phosphor/network/network.bb
@@ -23,4 +23,5 @@ S = "${WORKDIR}/git"
 do_install_append() {
         install -d ${D}/${sbindir}
         install ${S}/netman.py ${D}/${sbindir}
+        install ${S}/conf/network-manager.conf ${D}/${sysconfdir}
 }