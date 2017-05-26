openbmc/openbmc

Add target files that gpio monitor can call into
When GPIO monitor sees that a particular line is asserted,
it calls into one of the configured systemd targets and this
is one such. For now, the target just has host-quiesce and
will be enhanced to contain a service that logs an error.

Change-Id: I09afea5fcab3efce7bbfa5124af9b8cb2e0594bf
Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

meta-phosphor/common/recipes-core/systemd/obmc-targets.bb
@@ -35,7 +35,10 @@ HOST_SYNCH_TARGETS = "start-pre starting started stop-pre stopping stopped reset
 # - quiesce:  Target to enter on host boot failure
 # - shutdown: Tell host to shutdown, then stop system
 # - reset:   Services to check if host is running and update host "start" target
-HOST_ACTION_TARGETS = "start stop quiesce reset shutdown"
+# - termination: Target to run when host terminates. it is very much similar to
+#                quiesce target but the only delta is that this target contains
+#                multiple services and one of them is the quiesce target.
+HOST_ACTION_TARGETS = "start stop quiesce reset shutdown termination"
 
 CHASSIS_SYNCH_FMT = "obmc-power-{0}@.target"
 CHASSIS_ACTION_FMT = "obmc-chassis-power{0}@.target"
@@ -46,6 +49,11 @@ CHASSIS_LINK_SYNCH_FMT = "${CHASSIS_SYNCH_FMT}:obmc-power-{0}@{1}.target"
 CHASSIS_LINK_ACTION_FMT = "${CHASSIS_ACTION_FMT}:obmc-chassis-power{0}@{1}.target"
 HOST_LINK_SYNCH_FMT = "${HOST_SYNCH_FMT}:obmc-host-{0}@{1}.target"
 HOST_LINK_ACTION_FMT = "${HOST_ACTION_FMT}:obmc-host-{0}@{1}.target"
+
+QUIESCE_TMPL = "obmc-host-quiesce@.target"
+TERM_TGTFMT = "obmc-host-termination@{0}.target"
+QUIESCE_INSTFMT = "obmc-host-quiesce@{0}.target"
+TERM_QUIESCE_FMT = "${QUIESCE_TMPL}:${TERM_TGTFMT}.wants/${QUIESCE_INSTFMT}"
 
 SYSTEMD_SERVICE_${PN} += " \
         obmc-mapper.target \
@@ -64,3 +72,4 @@ SYSTEMD_LINK_${PN} += "${@compose_list(d, 'CHASSIS_LINK_SYNCH_FMT', 'CHASSIS_SYN
 SYSTEMD_LINK_${PN} += "${@compose_list(d, 'CHASSIS_LINK_ACTION_FMT', 'CHASSIS_ACTION_TARGETS', 'OBMC_CHASSIS_INSTANCES')}"
 SYSTEMD_LINK_${PN} += "${@compose_list(d, 'HOST_LINK_SYNCH_FMT', 'HOST_SYNCH_TARGETS', 'OBMC_HOST_INSTANCES')}"
 SYSTEMD_LINK_${PN} += "${@compose_list(d, 'HOST_LINK_ACTION_FMT', 'HOST_ACTION_TARGETS', 'OBMC_HOST_INSTANCES')}"
+SYSTEMD_LINK_${PN} += "${@compose_list(d, 'TERM_QUIESCE_FMT', 'OBMC_CHECKSTOP_INSTANCES')}"

openbmc/meta-phosphor

common/recipes-core/systemd/obmc-targets.bb
@@ -35,7 +35,10 @@ HOST_SYNCH_TARGETS = "start-pre starting started stop-pre stopping stopped reset
 # - quiesce:  Target to enter on host boot failure
 # - shutdown: Tell host to shutdown, then stop system
 # - reset:   Services to check if host is running and update host "start" target
-HOST_ACTION_TARGETS = "start stop quiesce reset shutdown"
+# - crash:   Target to run when host crashes. it is very much similar to
+#            quiesce target but the only delta is that this target contains
+#            multiple services and one of them is the quiesce target.
+HOST_ACTION_TARGETS = "start stop quiesce reset shutdown crash"
 
 CHASSIS_SYNCH_FMT = "obmc-power-{0}@.target"
 CHASSIS_ACTION_FMT = "obmc-chassis-power{0}@.target"
@@ -47,6 +50,11 @@ CHASSIS_LINK_ACTION_FMT = "${CHASSIS_ACTION_FMT}:obmc-chassis-power{0}@{1}.targe
 HOST_LINK_SYNCH_FMT = "${HOST_SYNCH_FMT}:obmc-host-{0}@{1}.target"
 HOST_LINK_ACTION_FMT = "${HOST_ACTION_FMT}:obmc-host-{0}@{1}.target"
 FAN_LINK_FMT = "obmc-fan-control-ready@.target:obmc-fan-control-ready@{0}.target"
+
+QUIESCE_TMPL = "obmc-host-quiesce@.target"
+CRASH_TGTFMT = "obmc-host-crash@{0}.target"
+QUIESCE_INSTFMT = "obmc-host-quiesce@{0}.target"
+CRASH_QUIESCE_FMT = "../${QUIESCE_TMPL}:${CRASH_TGTFMT}.wants/${QUIESCE_INSTFMT}"
 
 SYSTEMD_SERVICE_${PN} += " \
         obmc-mapper.target \
@@ -67,3 +75,4 @@ SYSTEMD_LINK_${PN} += "${@compose_list(d, 'CHASSIS_LINK_ACTION_FMT', 'CHASSIS_AC
 SYSTEMD_LINK_${PN} += "${@compose_list(d, 'HOST_LINK_SYNCH_FMT', 'HOST_SYNCH_TARGETS', 'OBMC_HOST_INSTANCES')}"
 SYSTEMD_LINK_${PN} += "${@compose_list(d, 'HOST_LINK_ACTION_FMT', 'HOST_ACTION_TARGETS', 'OBMC_HOST_INSTANCES')}"
 SYSTEMD_LINK_${PN} += "${@compose_list(d, 'FAN_LINK_FMT', 'OBMC_CHASSIS_INSTANCES')}"
+SYSTEMD_LINK_${PN} += "${@compose_list(d, 'CRASH_QUIESCE_FMT', 'OBMC_HOST_INSTANCES')}"