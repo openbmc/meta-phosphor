openbmc/meta-phosphor

commit 7def530c51f5372756eea09fd6d0589a7f7c22a8

meta-phosphor: mrw-api-native: set LICENSE field
Set LICENSE field explicitly.

Change-Id: I192b025878766fbfd02f36b9c8186e2f4e02c16e
Signed-off-by: Patrick Venture <venture@google.com>

recipes-phosphor/mrw/mrw-api-native.bb
@@ -1,10 +1,11 @@
 SUMMARY = "Phosphor OpenBMC machine readable workbook API modules"
 DESCRIPTION = "The API for the MRW XML generated by the Serverwiz tool"
 PR = "r1"
+LICENSE = "Apache-2.0"
+LIC_FILES_CHKSUM = "file://${WORKDIR}/LICENSE;md5=d2794c0df5b907fdace235a619d80314"
 
 S = "${WORKDIR}/scripts"
 
-inherit obmc-phosphor-license
 inherit native
 inherit perlnative
 inherit cpan-base

commit eb8f3d306e5e314f32fe1c838996470ed017c9de

phosphor: mrw-api: fetch all of serverwiz
Fetch all of the serverwiz repository so we get the LICENSE.

Change-Id: Ibd2b0d1afeb07e04b940bb82540b7024109087bf
Fixes: 7def530c51f5372756eea09fd6d0589a7f7c22a8
Fixes: openbmc/openbmc#3426
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

classes/mrw-rev.bbclass
@@ -1,4 +1,4 @@
-MRW_API_SRC_URI ?= "git://github.com/open-power/serverwiz.git;subpath=scripts"
+MRW_API_SRC_URI ?= "git://github.com/open-power/serverwiz.git"
 MRW_API_SRCREV ?= "60c8e10cbb11768cd1ba394b35cb1d6627efec42"
 
 MRW_TOOLS_SRC_URI ?= "git://github.com/openbmc/phosphor-mrw-tools"

recipes-phosphor/mrw/mrw-api-native.bb
@@ -2,9 +2,9 @@ SUMMARY = "Phosphor OpenBMC machine readable workbook API modules"
 DESCRIPTION = "The API for the MRW XML generated by the Serverwiz tool"
 PR = "r1"
 LICENSE = "Apache-2.0"
-LIC_FILES_CHKSUM = "file://${WORKDIR}/LICENSE;md5=d2794c0df5b907fdace235a619d80314"
+LIC_FILES_CHKSUM = "file://LICENSE;md5=d2794c0df5b907fdace235a619d80314"
 
-S = "${WORKDIR}/scripts"
+S = "${WORKDIR}/git"
 
 inherit native
 inherit perlnative
@@ -18,5 +18,5 @@ SRCREV = "${MRW_API_SRCREV}"
 
 do_install() {
     install -d ${D}${PERLLIBDIRS_class-native}/perl/site_perl/${PERLVERSION}/mrw
-    install -m 0755 Targets.pm ${D}${PERLLIBDIRS_class-native}/perl/site_perl/${PERLVERSION}/mrw/Targets.pm
+    install -m 0755 scripts/Targets.pm ${D}${PERLLIBDIRS_class-native}/perl/site_perl/${PERLVERSION}/mrw/Targets.pm
 }