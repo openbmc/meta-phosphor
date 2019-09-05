9c95a045a56edd573bc8bc0b5f1357780c94107b
Adriana Kobylak <anoo@us.ibm.com>
2019-09-05 13:58:53 -0500

    df-ext4/obmc-init.sh: Mount rwfs and overlay
    Mount the read-write fs over /var and overlay over /etc.
    
    Change-Id: I8b6d3d9b49fa7190a540b70f64725fcae99b1c76
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- recipes-phosphor/initrdscripts/files/df-ext4/obmc-init.sh
+++ recipes-phosphor/initrdscripts/files/df-ext4/obmc-init.sh
@@ -44,6 +44,11 @@ fi
 mkdir -p $rodir
 mount "$(get_root)" $rodir -t ext4 -o ro
 
+mount /dev/mapper/vg-rwfs $rodir/var -o rw
+rm -rf $rodir/var/persist/etc-work/
+mkdir -p $rodir/var/persist/etc $rodir/var/persist/etc-work
+mount overlay $rodir/etc -t overlay -o lowerdir=$rodir/etc,upperdir=$rodir/var/persist/etc,workdir=$rodir/var/persist/etc-work
+
 for f in $fslist; do
 	mount --move $f $rodir/$f
 done
