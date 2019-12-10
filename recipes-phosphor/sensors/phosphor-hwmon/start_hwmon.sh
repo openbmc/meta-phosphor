d2775aa6cdc6d995d10166114ed95f5130e709ca
William A. Kennington III <wak@google.com>
2019-12-09 17:52:08 -0800

    phosphor-hwmon: Fix unit escaping
    We aren't correctly escaping all of the specical characters based on the
    systemd spec for unit name escaping. systemd provides a utility to do
    this reliably, so use that instead of a custom implementation.
    
    Change-Id: Iaaa79b65858495ace1ff72a2d1a662fe318c117f
    Signed-off-by: William A. Kennington III <wak@google.com>

--- recipes-phosphor/sensors/phosphor-hwmon/start_hwmon.sh
+++ recipes-phosphor/sensors/phosphor-hwmon/start_hwmon.sh
@@ -17,7 +17,5 @@ then
     fi
 fi
 
-path="${path//:/--}"
-path="${path//-/\\x2d}"
-
-systemctl --no-block $action 'xyz.openbmc_project.Hwmon@'$path'.service'
+path="$(systemd-escape "$path")"
+systemctl --no-block "$action" "xyz.openbmc_project.Hwmon@$path.service"
