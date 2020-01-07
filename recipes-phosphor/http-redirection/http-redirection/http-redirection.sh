83f420dafad7600c5a4493f340edd16cc70fca5f
George Liu <liuxiwei@inspur.com>
2020-01-07 13:41:07 +0800

    Add http-redirection recipes
    Use nc command to listen to WebUI 80 port and implement redirect HTTP to
    HTTPS.
    
    Tested:
    GET or HEAD:
    1. Visit `http://100.2.39.132` redirect to `https://100.2.39.132`.
    2. Visit `http://100.2.39.132/login` redirect to
    `https://100.2.39.132/login`.
    POST:
    1. Visit `http://100.2.39.132` redirect to `https://100.2.39.132`.
    2. Visit `http://100.2.39.132/login` redirect to `https://100.2.39.132`.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I0a88fca1c07701f4909c790e2e3bd73e55696e9d

---
+++ recipes-phosphor/http-redirection/http-redirection/http-redirection.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+readRequest() {
+	bmcIp=""
+	bmcPath="/"
+	while true
+	do
+		read line
+		reqKey=`echo $line | awk -F '[ :]' '{print$1}' | sed $'s/\r//'`
+		reqKey="$(echo $reqKey | tr '[:lower:]' '[:upper:]')"
+
+		case "${reqKey}" in
+			GET|HEAD)
+				bmcPath=`echo $line | awk '{print$2}'`
+				;;
+			HOST)
+				bmcIp=`echo $line | awk -F : '{print$2}' | awk '{gsub(/^\s+|\s+$/, "");print}'`
+				;;
+			*)
+				break
+				;;
+		esac
+	done
+}
+
+sendRespData() {
+	cat <<-EOF
+	HTTP/1.1 308 Permanent Redirect
+	Location: https://$bmcIp$bmcPath
+	Content-Length: 23
+	Content-Type: text/html; charset=UTF-8
+	X_Frame_Options: DENY
+	Pragma: no-cache
+	Cache_Control: no-Store,no-Cache
+	X-XSS-Protection: 1; mode=block
+	X-Content-Type-Options: nosniff
+	Connection: close to HTTP/1.1
+
+	308 Permanent Redirect
+	EOF
+}
+
+main() {
+	while true
+	do
+		coproc nc -l -p 80
+		{
+			readRequest
+			if [ -n "${bmcIp}" ]; then
+                            sendRespData
+			fi
+		} <&${COPROC[0]} >&${COPROC[1]}
+
+		# When code runs here it means the above functions are finished.
+		# Usually nc will quit after sending the HTTP response and if the client behaves well.
+		# But when networks is not stable, the resposne may not be fully sent,
+		# and we should wait for a while before killing nc.
+		sleep 0.1
+		[ -n "$COPROC_PID" ] && echo "killing $COPROC_PID" && kill "$COPROC_PID"
+        done
+}
+
+main
