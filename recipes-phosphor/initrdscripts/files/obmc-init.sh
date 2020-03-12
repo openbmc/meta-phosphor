19e7cb2974bb94ed0b019bc96872a6e781034c02
Adriana Kobylak <anoo@us.ibm.com>
2020-03-12 11:12:59 -0500

    obmc-init.sh: Use u-boot env instead of password for emergency console
    The password prompt doesn't work anymore. Since it relied on the
    default password, remove the password prompt and just drop into
    a shell if a new enable-debug-sh u-boot environment value is set.
    The security implications are the same since the default
    password is public and there is a need to have access to u-boot
    to set the environment value.
    
    If the environment value is not set, kernel panic the system.
    
    Closes openbmc/openbmc#3649
    
    Tested: Verified kernel panic if environment value was not set, and
            if it was set the emergency console dropped into a shell and basic
            linux commands worked:
    
    Mounting read-write /dev/mtdblock5 filesystem failed.  Please fix and run
        mount /dev/mtdblock5 run/initramfs/rw -t jffs2 -o rw
    to to continue, or do change nothing to run from RAM for this boot.
    Try to manually fix.
    After fixing run exit to continue this script, or reboot -f to retry, or
    touch /takeover and exit to become PID 1 allowing editing of this script.
    /bin/sh: can't access tty; job control turned off
    / #
    
    Change-Id: Ibf84cb2235972b340b18c348406658074f35a506
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- recipes-phosphor/initrdscripts/files/obmc-init.sh
+++ recipes-phosphor/initrdscripts/files/obmc-init.sh
@@ -128,29 +128,36 @@ try_wget() {
 
 debug_takeover() {
 	echo "$@"
-	test -n "$@" && echo Enter password to try to manually fix.
-	cat << HERE
+
+	if grep -w enable-debug-sh "$optfile"
+	then
+		test -n "$@" && echo Try to manually fix.
+		cat << HERE
 After fixing run exit to continue this script, or reboot -f to retry, or
 touch /takeover and exit to become PID 1 allowing editing of this script.
 HERE
 
-	while ! sulogin && ! test -f /takeover
-	do
-		echo getty failed, retrying
-	done
+		while ! /bin/sh && ! test -f /takeover
+		do
+			echo getty failed, retrying
+		done
 
-	# Touch /takeover in the above getty to become pid 1
-	if test -e /takeover
-	then
-		cat << HERE
+		# Touch /takeover in the above getty to become pid 1
+		if test -e /takeover
+		then
+			cat << HERE
 
 Takeover of init requested.  Executing /bin/sh as PID 1.
 When finished exec new init or cleanup and run reboot -f.
 
 Warning: No job control!  Shell exit will panic the system!
 HERE
-		export PS1=init#\ 
-		exec /bin/sh
+			export PS1=init#\ 
+			exec /bin/sh
+		fi
+	else
+		echo "Triggering kernel panic!"
+		exit 1
 	fi
 }
 
@@ -208,7 +215,12 @@ echo rofs = $rofs $rofst   rwfs = $rwfs $rwfst
 
 if grep -w debug-init-sh $optfile
 then
-	debug_takeover "Debug initial shell requested by command line."
+	if grep -w enable-debug-sh "$optfile"
+	then
+		debug_takeover "Debug initial shell requested by command line."
+	else
+		echo "Need to also enable enable-debug-sh for debug shell."
+	fi
 fi
 
 if test "x$consider_download_files" = xy &&
@@ -392,11 +404,18 @@ elif ! mount $rwdev $rwdir -t $rwfst -o $rwopts
 then
 	msg="$(cat)" << HERE
 
-Mounting read-write $rwdev filesystem failed.  Please fix and run
-	mount $rwdev $rwdir -t $rwfst -o $rwopts
-to to continue, or do change nothing to run from RAM for this boot.
+Mounting read-write $rwdev filesystem failed.
 HERE
-	debug_takeover "$msg"
+	debug_msg="$(cat)" << HERE
+Please fix and run mount $rwdev $rwdir -t $rwfst -o $rwopts
+to continue, or do change nothing to run from RAM for this boot.
+HERE
+	if grep -w enable-debug-sh "$optfile"
+	then
+		debug_takeover "$msg $debug_msg"
+	else
+		debug_takeover "$msg"
+	fi
 fi
 
 rm -rf $work
@@ -411,7 +430,7 @@ do
 Unable to confirm /sbin/init is an executable non-empty file
 in merged file system mounted at /root.
 
-Change Root test failed!  Invoking emergency shell.
+Change Root test failed!
 HERE
 	debug_takeover "$msg"
 done
